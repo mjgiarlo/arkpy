4f11e70f5c18ac7d57e168c9819197e2609b0c66
mjgiarlo <leftwing@alumni.rutgers.edu>
2010-08-09 13:18:30 -0400

    updating the README

--- __init__.py
+++ __init__.py
@@ -1,52 +1,56 @@
-class ArkUtils
-  XDIGITS = %w( 0 1 2 3 4 5 6 7 8 9 b c d f g h j k m n p q r s t v w x z )
-  DIGITS = XDIGITS[0..9]
+import random
 
-  class << self
-    def mint( options={ :authority => nil, :template => 'dddddddd' } )
-      # mint a unique ARK within the given authority
-      # random template: eeddeeddk
-      #   e is an xdigit: 0123456789bcdfghjkmnpqrstvwxz
-      #   d is a digit:   0123456789
-      #   k is checkchar: special xdigit
- 
-      # first generate a name based on given template
-      name = generate_name( options[:template] )
 
-      # then prepend authority.number and the '/' character e.g., "11111/id"
-      ark = options[:authority].number.to_s + '/' + name 
+digits = [str(i) for i in range(0, 10)]
+xdigits = digits + ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'm', 'n',
+                    'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
 
-      # then generate a check character, if specified in template
-      name << generate_check( ark ) if options[:template][-1].chr == 'k'
+def mint(authority, template, prefix=''):
+    """ mint an ARK within an authority according to a template
+    """
+    # Penn State's ARK authority number is 42409
 
-      # return identifier w/ check char to caller
-      name
-    end
+    # random template: eeddeeddk
+    #   e is an xdigit: 0123456789bcdfghjkmnpqrstvwxz
+    #   d is a digit:   0123456789
+    #   k is checkchar: special xdigit
+    
+    # first generate a name based on given template
+    name = _generate_name(template)
 
-    def generate_name( template )
+    # then prepend the optional prefix (for additional namespacing)
+    name = prefix + name
+
+    # then prepend authority.number and the '/' character e.g., "11111/id"
+    ark = "%s/%s" % (authority, name)
+
+    # then generate a check character, if specified in template
+    if template[-1] == 'k':
+        ark += _generate_check(ark)
+
+    # return identifier w/ check char to caller
+    return ark
+
+def _generate_name(template):
       name = ''
-      template.split( '' ).each do |char|
-        next unless ['d', 'e'].include? char
-        digit_array = (char == 'e') ? XDIGITS : DIGITS
-        name << digit_array[rand(digit_array.length)]
-      end
-      name
-    end
+      for char in list(template):
+          if char not in ['d', 'e']:
+              continue
+          digit_array = xdigits if char == 'e' else digits
+          name += random.choice(digit_array)
+      return name
 
-    def generate_check( ark )
-      sum = position = 0
-      ark.split( '' ).each do |char|
-        ordinal = XDIGITS.index( char ) || 0
+def _generate_check(ark):
+    sum = 0
+    position = 0
+    for char in list(ark):
+        try:
+            ordinal = xdigits.index(char)
+        except ValueError:
+            ordinal = 0
         position += 1
-        sum += ordinal * position
-      end
-      XDIGITS[sum % XDIGITS.length]
-    end
+        sum += (ordinal * position)
+    return xdigits[sum % len(xdigits)]
 
-    def validate( ark )
-      generate_check( ark[0...-1] ) == ark[-1].chr 
-    end
-
-    private :generate_check, :generate_name
-  end
-end 
+def validate(ark):
+    return _generate_check(ark[:-1]) == ark[-1]
